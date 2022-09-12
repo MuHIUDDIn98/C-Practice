63d992ccd065c4792ed1179633dc3014a922b00a
laptopanik <muhiuddinanik98@gmail.com>
2022-09-13 00:39:15 +0600

    c++ commit1st

---
+++ programming C++/15th/loop.cpp
@@ -0,0 +1,6 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>
+<CodeBlocks_workspace_file>
+	<Workspace title="Workspace">
+		<Project filename="../class/different file class diclaration/my1st/my1st.cbp" />
+	</Workspace>
+</CodeBlocks_workspace_file>
