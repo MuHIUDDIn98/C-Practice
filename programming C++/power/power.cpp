e3dfb9eef1800af4e4fd7823a9e7ddac1f28ebde
laptopanik <muhiuddinanik98@gmail.com>
2022-11-05 14:21:36 +0600

    power raw data added

--- programming C++/power/power.cpp
+++ programming C++/power/power.cpp
@@ -10,6 +10,6 @@ main()
     cout<<"enter your power:";
     cin>>b;
    double power=pow(a,b);
-   cout<<"powered  value of enterd value is ="<<power;
+   cout<<"powered  value of enterd value is =  "<<power;
 
 }
